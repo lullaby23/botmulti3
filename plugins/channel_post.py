91d0e8915713f517549c0cec6bb3d344278e5446
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2022-02-09 03:44:29 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,220 +1,85 @@
 # (©)Codexbotz
 
-
-
+import asyncio
 
 from pyrogram import Client, filters
-
+from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-
-
-
 from bot import Bot
-
-from config import ADMINS
-
-from helper_func import encode, get_message_id
-
-
-
-
-
-
-
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("batch"))
-
-async def batch(client: Client, message: Message):
-
-    while True:
-
-        try:
-
-            first_message = await client.ask(
-
-                text="<b>Silahkan Forward Pesan/File Pertama dari Channel DataBase. (Forward with Qoute)</b>\n\n<b>atau Kirim Link Postingan dari Channel Database</b>",
-
-                chat_id=message.from_user.id,
-
-                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-
-                timeout=60,
-
-            )
-
-        except BaseException:
-
-            return
-
-        f_msg_id = await get_message_id(client, first_message)
-
-        if f_msg_id:
-
-            break
-
-        await first_message.reply(
-
-            "❌ <b>ERROR</b>\n\n<b>Postingan yang Diforward ini bukan dari Channel Database saya</b>",
-
-            quote=True,
-
-        )
-
-        continue
-
-
-
-
-    while True:
-
-        try:
-
-            second_message = await client.ask(
-
-                text="<b>Silahkan Forward Pesan/File Terakhir dari Channel DataBase. (Forward with Qoute)</b>\n\n<b>atau Kirim Link Postingan dari Channel Database</b>",
-
-                chat_id=message.from_user.id,
-
-                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-
-                timeout=60,
-
-            )
-
-        except BaseException:
-
-            return
-
-        s_msg_id = await get_message_id(client, second_message)
-
-        if s_msg_id:
-
-            break
-
-        await second_message.reply(
-
-            "❌ <b>ERROR</b>\n\n<b>Postingan yang Diforward ini bukan dari Channel Database saya</b>",
-
-            quote=True,
-
-        )
-
-        continue
-
-
-
-
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
-
-    base64_string = await encode(string)
-
-    link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup(
-
-        [
-
-            [
-
-                InlineKeyboardButton(
-
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
-
-                )
-
-            ]
-
-        ]
-
-    )
-
-    await second_message.reply_text(
-
-        f"<b>Link Sharing File Berhasil Di Buat:</b>\n\n{link}",
-
-        quote=True,
-
-        reply_markup=reply_markup,
-
-    )
-
-
-
-
-
-
-
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("genlink"))
-
-async def link_generator(client: Client, message: Message):
-
-    while True:
-
-        try:
-
-            channel_message = await client.ask(
-
-                text="<b>Silahkan Forward Pesan dari Channel DataBase. (Forward with Qoute)</b>\n\n<b>atau Kirim Link Postingan dari Channel Database</b>",
-
-                chat_id=message.from_user.id,
-
-                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-
-                timeout=60,
-
-            )
-
-        except BaseException:
-
-            return
-
-        msg_id = await get_message_id(client, channel_message)
-
-        if msg_id:
-
-            break
-
-        await channel_message.reply(
-
-            "❌ <b>ERROR</b>\n\n<b>Postingan yang Diforward ini bukan dari Channel Database saya</b>",
-
-            quote=True,
-
-        )
-
-        continue
-
-
-
-
-    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-
-    link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup(
-
-        [
-
-            [
-
-                InlineKeyboardButton(
-
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
-
-                )
-
-            ]
-
-        ]
-
-    )
-
-    await channel_message.reply_text(
-
-        f"<b>Link Sharing File Berhasil Di Buat:</b>\n\n{link}",
-
-        quote=True,
-
-        reply_markup=reply_markup,
-
-    )
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from helper_func import encode
+
+
+@Bot.on_message(
+    filters.private
+    & filters.user(ADMINS)
+    & ~filters.command(
+        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]
+    )
+)
+async def channel_post(client: Client, message: Message):
+    reply_text = await message.reply_text("<code>Tunggu Sebentar...</code>", quote=True)
+    try:
+        post_message = await message.copy(
+            chat_id=client.db_channel.id, disable_notification=True
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(
+            chat_id=client.db_channel.id, disable_notification=True
+        )
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("<b>Telah Terjadi Error...</b>")
+        return
+    converted_id = post_message.message_id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ]
+        ]
+    )
+
+    await reply_text.edit(
+        f"<b>Link Sharing File Berhasil Di Buat :</b>\n\n{link}",
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+    )
+
+    if not DISABLE_CHANNEL_BUTTON:
+        await post_message.edit_reply_markup(reply_markup)
+
+
+@Bot.on_message(
+    filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited
+)
+async def new_post(client: Client, message: Message):
+
+    if DISABLE_CHANNEL_BUTTON:
+        return
+
+    converted_id = message.message_id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ]
+        ]
+    )
+    try:
+        await message.edit_reply_markup(reply_markup)
+    except Exception as e:
+        print(e)
